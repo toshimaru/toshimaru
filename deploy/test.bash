8603bbc8206b0d12d8d9a0aa77ff1670c2fb0063
toshi <me@toshimaru.net>
2014-10-08 00:59:06 +0900

    more dynamic

--- deploy/test.bash
+++ deploy/test.bash
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# show bash version
 bash --version
-
-v="ho ge"; echo ${v^^}
 
 echo $CIRCLE_BRANCH
 var_upper="BRANCH_${CIRCLE_BRANCH^^}"
 echo $var_upper
+dynamic_var=${!var_upper}
 
-echo ${!var_upper}
-
+echo $dynamic_var
